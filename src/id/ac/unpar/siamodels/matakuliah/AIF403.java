699de5fdacc7a4f70dad4e098dfa1f5d00a304d6
Herfan Heryandi <herfanheryandi@gmail.com>
2015-11-17 02:33:14 +0700

    Menambahkan annotation

--- src/id/ac/unpar/siamodels/matakuliah/AIF403.java
+++ src/id/ac/unpar/siamodels/matakuliah/AIF403.java
@@ -2,7 +2,7 @@ package id.ac.unpar.siamodels.matakuliah;
 
 import id.ac.unpar.siamodels.MataKuliah;
 
-@MataKuliah(kode = "", nama = "", sks = 0)
+@MataKuliah(kode = "AIF403", nama = "Komputer & Masyarakat", sks = 2)
 public class AIF403 {
 
 }
