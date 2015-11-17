a7751dab99e706ddd50d96c11f68f2b84d4f1611
Herfan Heryandi <herfanheryandi@gmail.com>
2015-11-17 11:02:27 +0700

    Fix name MKU002

--- src/id/ac/unpar/siamodels/matakuliah/MUK002.java
+++
@@ -1,8 +0,0 @@
-package id.ac.unpar.siamodels.matakuliah;
-
-import id.ac.unpar.siamodels.MataKuliah;
-
-@MataKuliah(kode = "MUK002", nama = "Pendidikan Kewarganegaraan", sks = 2)
-public class MUK002 {
-
-}
